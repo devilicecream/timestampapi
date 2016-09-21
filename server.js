4af585e06e7afc1e57f1c199611b6092065b6598
WalterDa <walterdangalante@gmail.com>
2016-09-20 18:27:29 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+function formatDate(date) {
+  var months = ["January", "February", "March", "April", "May",  "June", "July", "August", "September", "October", "November", "December"];
+  return months[date.getMonth()] + ' ' + date.getDay() + ', ' + date.getFullYear();
+}
+
+app.get('/:time', function (req, res) {
+  if (req.params.time) {
+    var date = new Date(req.params.time);
+    if (date == "Invalid Date") {
+      res.send('{"unix": null, "natural": null}');
+    } else {
+      res.send('{"unix": ' + date.getTime() / 1000 + ', "natural": "' + formatDate(date) + '"}');
+    }
+  } else {
+    res.send('Hello World!');
+  }
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
